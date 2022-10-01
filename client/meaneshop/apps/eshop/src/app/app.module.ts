014499636a1e962139c30ea22d384efba8d6ea9f
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-10-01 23:17:51 +0300

    eshop footer & header added

--- client/meaneshop/apps/eshop/src/app/app.module.ts
+++ client/meaneshop/apps/eshop/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent],
-  imports: [BrowserModule , RouterModule.forRoot([
-    {path : '',component: HomePageComponent},
-    {path : 'products' , component: ProductListComponent}])
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
