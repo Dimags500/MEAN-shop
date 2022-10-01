38f8cf500f0f11e09245b5b86b5d3b579b8cdfc4
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-10-01 22:47:50 +0300

    nx monoRepo added

---
+++ client/meaneshop/apps/eshop/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+
+@NgModule({
+  declarations: [AppComponent, HomePageComponent, ProductListComponent],
+  imports: [BrowserModule , RouterModule.forRoot([
+    {path : '',component: HomePageComponent},
+    {path : 'products' , component: ProductListComponent}])
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
