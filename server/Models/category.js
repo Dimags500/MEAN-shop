bb726ac609336560f7b7ed7e14cdb3e6f3b075a0
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-10-04 10:53:31 +0300

    full category modal

--- server/Models/category.js
+++ server/Models/category.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-  name: String,
-  color: String,
-  icon: String,
+  name: {
+    type: String,
+    require: true,
+  },
+  color: {
+    type: String,
+  },
+  icon: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
 });
 
 const Category = mongoose.model("Category", categorySchema);
