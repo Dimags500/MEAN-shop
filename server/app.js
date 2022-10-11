02f268ccbabe44348a2ba90d39ff2110cd538e90
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-10-11 21:28:56 +0300

    cors

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 const api = process.env.API_URL;
 
 // middlewares
-//app.use(cors());
+app.use(cors());
 app.options("*", cors());
 app.use(express.json());
 app.use(express.static("public"));
